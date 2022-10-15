18499d008794a48d64011a79b29433a695398c87
Rishabh Maurya <rishabhmaurya7654@gmail.com>
2022-10-15 19:07:33 +0530

    commit
    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
 import '../styles/globals.css'
+import {Layout} from '../components'
+import { Toaster } from 'react-hot-toast'
+import '../styles/globals.css'
+import {StateContext} from '../context/StateContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Toaster/>
+     <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    </StateContext>
+    
+  )
 }
 
 export default MyApp
